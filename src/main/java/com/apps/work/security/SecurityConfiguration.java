82b98259973d4658783091a856f505f787965641
saeedarcana <saeedarcana@gmail.com>
2020-07-20 17:07:06 +0500

    Testing Sessions

--- src/main/java/com/apps/work/security/SecurityConfiguration.java
+++ src/main/java/com/apps/work/security/SecurityConfiguration.java
@@ -1,15 +1,25 @@
 package com.apps.work.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.session.SessionRegistry;
+import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.web.authentication.session.*;
+import org.springframework.security.web.session.ConcurrentSessionFilter;
+import org.springframework.security.web.session.HttpSessionEventPublisher;
+import org.springframework.security.web.session.SimpleRedirectSessionInformationExpiredStrategy;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -20,20 +30,60 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             throws Exception {
         auth.inMemoryAuthentication().withUser("admin").password("{noop}admin")
                 .roles("USER", "ADMIN");
+        auth.inMemoryAuthentication().withUser("user").password("{noop}user")
+                .roles("USER");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.addFilter(concurrentSessionFilter());
         http.authorizeRequests().antMatchers("/login").permitAll()
-                .antMatchers("/", "/*todo*/**").access("hasRole('USER')").and()
-                .formLogin();
+                .antMatchers("/", "/*todo*/**").access("hasRole('USER')")
+                .and() .formLogin()
+                .and().sessionManagement()
+                .maximumSessions(1).sessionRegistry(sessionRegistry()).expiredUrl("/logout")
+                .and().sessionAuthenticationStrategy(compositeSessionAuthenticationStrategy());
     }
 
-    /*    @SuppressWarnings("deprecation")
-        @Bean
-        public static NoOpPasswordEncoder passwordEncoder() {
-            return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
-        }*/
+    @Bean
+    public SessionRegistry sessionRegistry() {
+        return new SessionRegistryImpl();
+    }
+    @Bean
+    public ConcurrentSessionFilter concurrentSessionFilter() {
+        return new ConcurrentSessionFilter(sessionRegistry(), new SimpleRedirectSessionInformationExpiredStrategy("/"));
+    }
+    @Bean
+    public ServletListenerRegistrationBean<HttpSessionEventPublisher> httpSessionEventPublisher() {
+        return new ServletListenerRegistrationBean<HttpSessionEventPublisher>(new HttpSessionEventPublisher());
+    }
 
+    @Bean
+    public ConcurrentSessionControlAuthenticationStrategy concurrentSessionControlAuthenticationStrategy() {
 
+        ConcurrentSessionControlAuthenticationStrategy concurrentSessionControlAuthenticationStrategy =
+                new ConcurrentSessionControlAuthenticationStrategy(sessionRegistry());
+        concurrentSessionControlAuthenticationStrategy.setExceptionIfMaximumExceeded(true);
+        concurrentSessionControlAuthenticationStrategy.setMaximumSessions(1);
+        return concurrentSessionControlAuthenticationStrategy;
+    }
+
+    @Bean
+    public SessionFixationProtectionStrategy sessionFixationProtectionStrategy() {
+        return new SessionFixationProtectionStrategy();
+    }
+    @Bean
+    public RegisterSessionAuthenticationStrategy registerSessionAuthenticationStrategy() {
+        return new RegisterSessionAuthenticationStrategy(sessionRegistry());
+    }
+
+    @Bean
+    public CompositeSessionAuthenticationStrategy compositeSessionAuthenticationStrategy() {
+        List<SessionAuthenticationStrategy> sessionAuthenticationStrategyList = new ArrayList<>();
+        sessionAuthenticationStrategyList.add(concurrentSessionControlAuthenticationStrategy());
+        sessionAuthenticationStrategyList.add(sessionFixationProtectionStrategy());
+        sessionAuthenticationStrategyList.add(registerSessionAuthenticationStrategy());
+
+        return new CompositeSessionAuthenticationStrategy(sessionAuthenticationStrategyList);
+    }
 }
